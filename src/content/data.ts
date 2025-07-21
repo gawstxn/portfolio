911a72f890023a9fd3a654cce1f8bdebb95420db
gawston <naruenat.privatez@gmail.com>
2025-07-21 17:28:46 +0700

    Add all projects page

--- src/content/data.ts
+++ src/content/data.ts
@@ -16,7 +16,7 @@ export const projects = [
         name: "Gawsdle",
         desc: "Independent Project",
         details: [
-            "Gawsdle is a daily character-guessing game inspired by Loldle, designed for fans of Genshin Impact and Honkai: Star Rail. Players must guess a character based on limited information and hints such as element, weapon type, rarity, or region."
+            "Gawsdle is a character-guessing game inspired by Loldle, designed for fans of Genshin Impact and Honkai: Star Rail. Players must guess a character based on limited information and hints such as element, weapon type, rarity, or region."
         ],
         stack: [
             "Next.js", "TailwindCSS"
@@ -36,7 +36,19 @@ export const projects = [
         ],
         year: 2024,
         link: "https://github.com/gawstxn/Front-End-Prisoner"
-    }
+    },
+    {
+        name: "Coming soon . . . .",
+        desc: "Secret project",
+        details: [
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        ],
+        stack: [
+            "Lorem", "Lorem", "Lorem"
+        ],
+        year: 2025,
+        link: "#"
+    },
 ]
 
 export const educations = [
